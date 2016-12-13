dad2fbaa6f28b86bb1f667a18df319abdaaa8d52
coco <corentin.regnier59@gmail.com>
2016-12-13 16:18:45 +0100

    Add front page white book consultation

--- themes/livresblancs/assets/js/livresblancs.script.js
+++ themes/livresblancs/assets/js/livresblancs.script.js
@@ -15,18 +15,4 @@
         dots: true,
     });
 
-    $('#edit-field-evaluation').addClass('rating');
-    var i;
-
-    console.log('test0');
-    for (i=1; i <= 10; i++) {
-        console.log('test1');
-        if( i % 2 == 0) {
-            console.log('test2');
-            $('.form-item-field-evaluation label').addClass('full');
-        } else {
-            console.log('test2.1');
-            $('.form-item-field-evaluation label').addClass('half');
-        }
-    }
 })(jQuery);
